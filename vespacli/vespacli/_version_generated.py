086fcfcd9e530602b6c4ea73ee79176f336e840f
thomasht86 <24563696+thomasht86@users.noreply.github.com>
2024-09-26 01:43:50 +0000

    Update Vespa CLI version to 8.413.11

--- vespacli/vespacli/_version_generated.py
+++ vespacli/vespacli/_version_generated.py
@@ -1,1 +1,1 @@
-vespa_version = "8.387.10"  # Automatically updated by github actions bot vespacli/_version_generated.py
+vespa_version = "8.413.11"  # Automatically updated by github actions bot vespacli/_version_generated.py
